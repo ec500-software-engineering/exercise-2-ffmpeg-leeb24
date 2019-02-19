439d83d815c357d290510a344573faae85bfd5af
ByoungSul lee <byoungsullee@ByoungSului-MacBook-Pro.local>
2019-02-19 00:09:43 -0500

    Multiprocessing and Queue for video inputs

--- FFMPEG.py
+++ FFMPEG.py
@@ -4,29 +4,30 @@ import queue
 import subprocess
 # import timeit
 import time
-# import multiprocessing as mp
+import multiprocessing as mp
 
-vidQ = queue.Queue()
-
-
-def convert_video(video_input, video_output):
-
-    cmds = ['ffmpeg', '-i',video_input ,'-r' ,'30', '-s','hd720',video_output]
-    cmds2 = ['ffmpeg', '-i',video_input ,'-r' ,'30', '-s','hd720','res.mp4']
-    sub1 = subprocess.Popen(cmds)
-    sub2 = subprocess.Popen(cmds2)
-
-    sub1.wait()
-    sub2.wait()
+vidQ = []
 
 
-    sub1.kill()
-    print("Sub1 killed")
-    sub2.kill()
-    print("Sub2 killed")
+def convert_video(video_input):
+	parsed = video_input.split('.')
+	output_vid = parsed[0]+'res.'+parsed[1]
+	print(output_vid)
+	cmds =['ffmpeg', '-i',video_input ,'-r' ,'30', '-s','hd720',output_vid]
+	sub1 = subprocess.Popen(cmds)
 
+	sub1.wait()
+	sub1.kill()
+	print("Sub1 killed")
+ 	
 
+# Set-up Muti pool 
+pool = mp.Pool(processes=2) ## 2 cores CPU
+
+vidQ.append("test.mp4")
+vidQ.append("test2.mp4")
 
 start_time = time.time()
-convert_video('test.MP4', 'testres.mp4')
+results = [pool.apply(convert_video, args=(video,)) for video in vidQ]
+
 print("--- %s seconds ---" % (time.time() - start_time))
