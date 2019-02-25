54fa3d13f3f19eb8f302d9c8c9072ad30db8ab81
ByoungSul lee <byoungsullee@dhcp-wifi-8021x-155-41-75-197.bu.edu>
2019-02-24 21:21:46 -0500

    Threading

--- FFMPEG.py
+++ FFMPEG.py
@@ -1,30 +1,57 @@
 import subprocess
 import time
 import multiprocessing as mp
+import queue
+import threading
 
+def convert_video_720p(video_input):
+	
+		parsed = video_input[0].split('.')
+		output_vid = parsed[0]+'res.'+parsed[1]
+		cmds =['ffmpeg', '-i',video_input[0] ,'-s','hd720',output_vid]
+		subprocess.call(cmds)
 
-def convert_video(video_input):
-	parsed = video_input.split('.')
-	output_vid = parsed[0]+'res.'+parsed[1]
-	cmds =['ffmpeg', '-i',video_input ,'-s','hd720',output_vid]
-	subprocess.Popen(cmds)
+		return print("Conversion Finished")
 
-def input_Queue(vidQ):
-	start_time = time.time()
+def convert_video_480p(video_input):
 
-	results = [pool.apply(convert_video, args=(video,)) for video in vidQ]
-	print(results)
-	print("--- %s seconds ---" % (time.time() - start_time))
+		parsed = video_input[0].split('.')
+		output_vid = parsed[0]+'res.'+parsed[1]
+		cmds =['ffmpeg', '-i',video_input[0] ,'-s','hd480',output_vid]
+		subprocess.call(cmds)
+
+		return print("Conversion Finished")
+
+# def input_Queue(vidQ):
+# 	start_time = time.time()
+# 	results = [pool.apply(convert_video_720p, args=(video,)) for video in vidQ]
+# 	print(results)
+# 	print("--- %s seconds ---" % (time.time() - start_time))
+# def put_to_Queue720():
+# 	vidQueue720.put(["test.mp4",720])
+# 	convert_video_720p(vidQueue720)
+
+# def put_to_Queue480():
+# 	vidQueue480.put(["test2.mp4",480])
+# 	convert_video_480p(vidQueue480)
 
 
 # Set-up Muti pool 
-vidQueue = []
+
+vidQueue720 = []
+vidQueue480 = [] 
 pool = mp.Pool(processes=2) ## 2 cores CPU
-vidQueue.append("test.mp4")
-vidQueue.append("test2.mp4")
+vidQueue720.append(["test.mp4",720])
+vidQueue480.append(["test2.mp4",480])
 
+start_time = time.time()
 
-input_Queue(vidQueue)
+thread1 =threading.Thread(target=convert_video_720p,args=(vidQueue720))
+thread2 =threading.Thread(target=convert_video_480p,args=(vidQueue480))
+thread1.start()
+thread2.start()
+print("--- %s seconds ---" % (time.time() - start_time))
+
 
 ##Exercise 2 
 ## Make a code that takes in multiple video inputs and output converted video files.
